b46dd4db034fc8e6e9bbd24671c769b123fa6f87
Ayoze <elpayov2@gmail.com>
2022-02-01 08:52:14 +0000

    scripts

---
+++ ut6/a2/scripts/bash/eleccion28.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo "
+Qué desea hacer?
+
+1) Crear usuarios (c)
+2) Borrar usuaros (b)
+3) Salir (s)
+"
+
+read OPTION
+
+if [ "$OPTION" = "c" ]; then
+    bash /home/ayoze/users28/crear-usuarios28.sh
+elif [ "$OPTION" = "b" ]; then
+    bash /home/ayoze/users28/borrar-usuarios28.sh
+elif [ "$OPTION" = "s" ]; then
+    echo "Saliendo del programa"
+    exit 0
+else
+    echo "La opción introducida es incorrecta"
+    exit 1
+fi
+
